f417bbf9d232772de2f97a721af2ffa0b0aa7846
Akash Choudhary <choudhary98akash@gmail.com>
2024-09-29 16:20:39 +0530

    adding form to github live

---
+++ script.js
@@ -0,0 +1,57 @@
+<div class="tshirt active" id="tshirt1">
+                <img src="T1.webp" alt="T-shirt 1">
+            </div>
+            <div class="tshirt" id="tshirt2">
+                <img src="T2.webp" alt="T-shirt 2">
+            </div>
+            <div class="tshirt" id="tshirt3">
+                <img src="T3.webp" alt="T-shirt 3">
+            </div>
+            <div class="tshirt" id="tshirt4">
+                <img src="T4.webp" alt="T-shirt 4">
+            </div>
+            <div class="tshirt" id="tshirt1">
+                <img src="T5.webp" alt="T-shirt 1">
+            </div>
+            <div class="tshirt" id="tshirt2">
+                <img src="T6.webp" alt="T-shirt 2">
+            </div>
+            <div class="tshirt" id="tshirt3">
+                <img src="T7.jpg" alt="T-shirt 3">
+            </div>
+            <div class="tshirt" id="tshirt4">
+                <img src="T8.webp" alt="T-shirt 4">
+            </div>
+            <div class="tshirt" id="tshirt1">
+                <img src="T9.webp" alt="T-shirt 1">
+            </div>
+            <div class="tshirt" id="tshirt2">
+                <img src="T10.webp" alt="T-shirt 2">
+            </div>
+            <div class="tshirt" id="tshirt3">
+                <img src="T11.webp" alt="T-shirt 3">
+            </div>
+            <div class="tshirt" id="tshirt4">
+                <img src="T12.webp" alt="T-shirt 4">
+            </div>
+            <div class="tshirt" id="tshirt1">
+                <img src="T13.webp" alt="T-shirt 1">
+            </div>
+            <div class="tshirt" id="tshirt2">
+                <img src="T15.webp" alt="T-shirt 2">
+            </div>
+            <div class="tshirt" id="tshirt3">
+                <img src="T16.webp" alt="T-shirt 3">
+            </div>
+            <div class="tshirt" id="tshirt4">
+                <img src="T17.jpg" alt="T-shirt 4">
+            </div>
+            <div class="tshirt" id="tshirt1">
+                <img src="T18.webp" alt="T-shirt 1">
+            </div>
+            <div class="tshirt" id="tshirt2">
+                <img src="T19.webp" alt="T-shirt 2">
+            </div>
+            <div class="tshirt" id="tshirt3">
+                <img src="T20.webp" alt="T-shirt 3">
+            </div>
